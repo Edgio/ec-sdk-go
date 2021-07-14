c4f872f1badcb6172229451c14689f31e6a7d604
ssaluja4 <86080722+ssaluja4@users.noreply.github.com>
2021-07-14 11:22:13 -0700

    [DEN-6] WAF - Gets All Custom Rule Sets implementation (#11)
    * waf - Gets All Custom Rule Sets implementation
    
    * removed duplicate code for date time formatting
    
    Co-authored-by: Shikha Saluja <ssaluja@ad.ouroath.com>

---
+++ edgecast/waf/custom_rule.go
@@ -0,0 +1,42 @@
+// Copyright Verizon Media, Licensed under the terms of the Apache 2.0 license. See LICENSE file in project root for terms.
+// Package waf provides an API for managing Web Application Firewall for the EdgeCast CDN.
+// WAF  provides a layer of security between security threats and your external web infrastructure.
+package waf
+
+import (
+	"fmt"
+)
+
+type CustomRuleSet struct {
+	//Indicates the system-defined ID for the custom rule set.
+	Id string `json:"id"`
+
+	//Indicates the date and time at which the custom rule was last modified.
+	//Syntax:
+	//MM/DD/YYYYhh:mm:ss [AM|PM]
+	LastModifiedDate string `json:"last_modified_date"`
+
+	//Indicates the name of the custom rule set.
+	Name string `json:"name"`
+}
+
+//Retrieves a list of custom rule sets. A custom rule set allows you to define custom threat assessment criterion.
+func (svc *WAFService) GetAllCustomRuleSets(accountNumber string) ([]CustomRuleSet, error) {
+	url := fmt.Sprintf("/v2/mcc/customers/%s/waf/v1.0/rules", accountNumber)
+
+	request, err := svc.Client.BuildRequest("GET", url, nil)
+
+	if err != nil {
+		return nil, fmt.Errorf("GetAllCustomRuleSets: %v", err)
+	}
+
+	var customRuleSets = &[]CustomRuleSet{}
+
+	_, err = svc.Client.SendRequest(request, &customRuleSets)
+
+	if err != nil {
+		return nil, fmt.Errorf("GetAllCustomRuleSets: %v", err)
+	}
+
+	return *customRuleSets, nil
+}
---
+++ example/getAllCustomRuleSets.go
@@ -0,0 +1,43 @@
+// Copyright Verizon Media, Licensed under the terms of the Apache 2.0 license . See LICENSE file in project root for terms.
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/VerizonDigital/ec-sdk-go/edgecast"
+	"github.com/VerizonDigital/ec-sdk-go/edgecast/waf"
+)
+
+// Retrieves a list of custom rule sets
+//
+// Usage:
+// go run getAllCustomRuleSets.go -api-token "<api-token>
+func main() {
+
+	apiToken := flag.String("api-token", "", "API Token provided to you")
+	accountNumber := flag.String("account-number", "", "Account number you wish to retrieve all Managed Rules for")
+
+	flag.Parse()
+
+	wafConfig := waf.NewConfig(*apiToken)
+	wafConfig.Logger = edgecast.NewStandardLogger()
+	wafService, err := waf.New(wafConfig)
+
+	if err != nil {
+		fmt.Printf("error creating WAF Service: %v\n", err)
+		return
+	}
+
+	//Get all Custom Rule Sets example
+	customRuleSets, err := wafService.GetAllCustomRuleSets(*accountNumber)
+
+	if err != nil {
+		fmt.Printf("Error retrieving all custom rule sets: %v\n", err)
+		return
+	}
+
+	for _, rule := range customRuleSets {
+		fmt.Println(rule)
+	}
+}
