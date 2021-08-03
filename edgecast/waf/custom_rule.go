06d9b28600f136d9932d970bd89ef79e4c086d7c
Steven Paz <steven@stevenpaz.com>
2021-08-03 16:39:32 -0700

    GetCustomRuleRate. Comment formatting. Simplified Examples.

--- edgecast/waf/custom_rule.go
+++ edgecast/waf/custom_rule.go
@@ -2,30 +2,17 @@
 
 package waf
 
-import (
-	"fmt"
-)
-
 /*
 	This file contains operations and types specific to WAF Custom Rule Sets.
 */
 
-// DeleteCustomRuleResponse contains the response from the WAF API when deleting a new custom rule
-type DeleteCustomRuleResponse struct {
+import (
+	"fmt"
+)
 
-	// ID indicates the generated ID for the newly deleted Rule
-	ID string
-
-	WAFResponse
-}
-
-// AddCustomRuleResponse contains the response from the WAF API when adding a new custom rule
-type AddCustomRuleResponse struct {
-	AddRuleResponse
-}
-
+// CustomRuleSetDetail is a detailed representation of a custom rule set.
 // A custom rule set defines custom threat assessment criteria.
-type CustomRule struct {
+type CustomRuleSetDetail struct {
 
 	// Contains custom rules. Each directive object defines a custom rule via the sec_rule object.
 	// You may create up to 10 custom rules.
@@ -35,17 +22,18 @@ type CustomRule struct {
 	Name string `json:"name,omitempty"`
 }
 
-//Contains custom rules. Each directive object defines a custom rule via the sec_rule object.
+// Contains custom rules. Each directive object defines a custom rule via the sec_rule object.
 type Directive struct {
 
-	//Defines a custom rule
+	// Defines a custom rule
 	SecRule SecRule `json:"sec_rule"`
 }
 
 // Defines a custom rule
 type SecRule struct {
 
-	// Determines whether the string identified in a variable object will be transformed and the metadata that will be assigned to malicious traffic.
+	// Determines whether the string identified in a variable object will
+	// be transformed and the metadata that will be assigned to malicious traffic.
 	Action Action `json:"action"`
 
 	// Contains additional criteria that must be satisfied to identify a malicious request.
@@ -61,14 +49,19 @@ type SecRule struct {
 	Variables []Variable `json:"variable"`
 }
 
-// The action object determines whether the value derived from the request element identified in a variable object will be transformed and the metadata that will be used to identify malicious traffic.
+/*
+	Action determines whether the value derived from the
+	request element identified in a variable object will be transformed
+	and the metadata that will be used to identify malicious traffic.
+*/
 type Action struct {
 
 	/*
 		Determines the custom ID that will be assigned to this custom rule. This custom ID is exposed via the Threats Dashboard.
 
 		Valid values fall within this range: 66000000 - 66999999
-			Note: This field is only applicable for the action object that resides in the root of the sec_rule object.
+
+		Note: This field is only applicable for the action object that resides in the root of the sec_rule object.
 
 		Default Value: Random number
 	*/
@@ -76,11 +69,12 @@ type Action struct {
 
 	/*
 		Determines the rule message that will be assigned to this custom rule. This message is exposed via the Threats Dashboard.
-			Note: This field is only applicable for the action object that resides in the root of the sec_rule object.
+
+		Note: This field is only applicable for the action object that resides in the root of the sec_rule object.
 
 		Default Value: Blank
 	*/
-	MSG string `json:"msg,omitempty"`
+	Message string `json:"msg,omitempty"`
 
 	/*
 		Determines the set of transformations that will be applied to the value derived from the request element identified in a variable object (i.e., source value).
@@ -90,21 +84,26 @@ type Action struct {
 
 			NONE: Indicates that the source value should not be modified.
 			LOWERCASE: Indicates that the source value should be converted to lowercase characters.
-			URLDECODE: Indicates that the source value should be URL decoded. This transformation is useful when the source value has been URL encoded twice.
+			URLDECODE: Indicates that the source value should be URL decoded. This transformation
+						is useful when the source value has been URL encoded twice.
 			REMOVENULLS: Indicates that null values should be removed from the source value.
 
-		Note: A criterion is satisfied if the source value or any of the modified string values meet the conditions defined by the operator object.
+		Note: A criterion is satisfied if the source value or any of the modified string values
+		meet the conditions defined by the operator object.
 	*/
-	T []string `json:"t,omitempty"`
+	Transformations []string `json:"t,omitempty"`
 }
 
-// Each object within the chained_rule array describes an additional set of criteria that must be satisfied in order to identify a malicious request.
+// Each object within the chained_rule array describes an additional set of
+// criteria that must be satisfied in order to identify a malicious request.
 type ChainedRule struct {
 
-	// Determines whether the string value derived from the request element identified in a variable object will be transformed and the metadata that will be used to identify malicious traffic.
+	// Determines whether the string value derived from the request element identified
+	// in a variable object will be transformed and the metadata that will be used to identify malicious traffic.
 	Action Action `json:"action"`
 
-	// Indicates the comparison that will be performed on the string value(s) derived from the request element(s) defined within the variable array.
+	// Indicates the comparison that will be performed on the string value(s) derived
+	// from the request element(s) defined within the variable array.
 	Operator Operator `json:"operator"`
 
 	// Identifies each request element for which a comparison will be made.
@@ -116,34 +115,43 @@ type Variable struct {
 
 	/*
 		Determines the request element that will be assessed.
-		Valid values are:
-		ARGS_POST | GEO | QUERY_STRING | REMOTE_ADDR | REQUEST_BODY | REQUEST_COOKIES | REQUEST_HEADERS | REQUEST_METHOD | REQUEST_URI
 
-		Note: If a request element consists of one or more key-value pairs, then you may identify a key via a match object. If is_count has been disabled, then you may identify a specific value via the operator object.
+		Valid values are:
+			ARGS_POST | GEO | QUERY_STRING | REMOTE_ADDR | REQUEST_BODY | REQUEST_COOKIES | REQUEST_HEADERS | REQUEST_METHOD | REQUEST_URI
+
+		Note: If a request element consists of one or more key-value pairs, then you
+		may identify a key via a match object.
+		If is_count has been disabled, then you may identify a specific value via the operator object.
 	*/
 	Type string `json:"type"`
 
-	//Contains comparison settings for the request element identified by the type property.
+	// Contains comparison settings for the request element identified by the type property.
 	Matches []Match `json:"match,omitempty"`
 
 	/*
-		Determines whether a comparison will be performed between the operator object and a string value or the number of matches found.
+		Determines whether a comparison will be performed between the operator object and
+		a string value or the number of matches found.
+
 		Valid values are:
 
-		true: A counter will increment whenever the request element defined by this variable object is found. The operator object will perform a comparison against this number.
-			Note: If you enable is_count, then you must also set the type property to EQ.
+		true: A counter will increment whenever the request element defined by this variable object
+		is found. The operator object will perform a comparison against this number.
+		** Note: If you enable is_count, then you must also set the type property to EQ.**
 
-		false: The operator object will perform a comparison against the string value derived from the request element defined by this variable object.
+		false: The operator object will perform a comparison against the string value derived from
+		the request element defined by this variable object.
 
 	*/
 	IsCount bool `json:"is_count,omitempty"`
 }
 
-//The operator object describes the comparison that will be performed on the request element(s) defined within a variable object using its properties:
+// Operator describes the comparison that will be performed on the request element(s) defined
+// within a variable object using its properties:
 type Operator struct {
 
 	/*
-		Indicates whether a condition will be satisfied when the value derived from the request element defined within a variable object matches or does not match the value property.
+		Indicates whether a condition will be satisfied when the value derived from
+		the request element defined within a variable object matches or does not match the value property.
 
 		Valid values are:
 
@@ -153,10 +161,12 @@ type Operator struct {
 	IsNegated bool `json:"is_negated,omitempty"`
 
 	/*
-		Indicates how the system will interpret the comparison between the value property and the value derived from the request element defined within a variable object.
+		Indicates how the system will interpret the comparison between the value property
+		and the value derived from the request element defined within a variable object.
+
 		Valid values are:
 
-			RX: Indicates that the string value derived from the request element must satisfy the regular expression defined in the value property.
+			RX:Indicates that the string value derived from the request element must satisfy the regular expression defined in the value property.
 			STREQ: Indicates that the string value derived from the request element must be an exact match to the value property.
 			CONTAINS: Indicates that the value property must contain the string value derived from the request element.
 			BEGINSWITH: Indicates that the value property must start with the string value derived from the request element.
@@ -168,12 +178,16 @@ type Operator struct {
 	Type string `json:"type"`
 
 	/*
-		Indicates a value that will be compared against the string or number value derived from the request element defined within a variable object.
+		Indicates a value that will be compared against the string or number value derived from
+		the request element defined within a variable object.
 
-		Note: If you are identifying traffic via a URL path (REQUEST_URI), then you should specify a URL path pattern that starts directly after the hostname. Exclude a protocol or a hostname when defining this property.
-				Sample values:
-				/marketing
-				/800001/mycustomerorigin
+		Note: If you are identifying traffic via a URL path (REQUEST_URI), then you should
+		specify a URL path pattern that starts directly after the hostname. Exclude a protocol
+		or a hostname when defining this property.
+
+		Sample values:
+			/marketing
+			/800001/mycustomerorigin
 	*/
 	Value string `json:"value,omitempty"`
 }
@@ -182,7 +196,8 @@ type Operator struct {
 type Match struct {
 
 	/*
-		Determines whether this condition is satisfied when the request element identified by the variable object is found or not found.
+		Determines whether this condition is satisfied when the request element
+		identified by the variable object is found or not found.
 
 			True: Not found
 			False: Found
@@ -202,26 +217,22 @@ type Match struct {
 
 		Example:
 
-			If the type property is set to REQUEST_HEADERS and this property is set to User-Agent, then this match condition is restricted to the User-Agent request header. If the value property is omitted, then this match condition applies to all request headers.
+		If the type property is set to REQUEST_HEADERS and this property is set to User-Agent,
+		then this match condition is restricted to the User-Agent request header.
+
+		If the value property is omitted, then this match condition applies to all request headers.
 	*/
 	Value string `json:"value,omitempty"`
 }
 
-type CustomRuleSet struct {
-	//Indicates the system-defined ID for the custom rule set.
-	Id string `json:"id"`
-
-	//Indicates the date and time at which the custom rule was last modified.
-	//Syntax:
-	//MM/DD/YYYYhh:mm:ss [AM|PM]
-	LastModifiedDate string `json:"last_modified_date"`
-
-	//Indicates the name of the custom rule set.
-	Name string `json:"name"`
+// AddCustomRuleSetResponse represents the response from the WAF API when adding a new custom rule
+type AddCustomRuleSetResponse struct {
+	AddRuleResponse
 }
 
 // Creates a custom rule set that defines custom threat assessment criteria.
-func (svc *WAFService) AddCustomRuleSet(customRuleSet CustomRule, accountNumber string) (*AddCustomRuleResponse, error) {
+func (svc *WAFService) AddCustomRuleSet(customRuleSet CustomRuleSetDetail, accountNumber string) (*AddCustomRuleSetResponse, error) {
+
 	url := fmt.Sprintf("/v2/mcc/customers/%s/waf/v1.0/rules", accountNumber)
 
 	request, err := svc.Client.BuildRequest("POST", url, customRuleSet)
@@ -230,7 +241,7 @@ func (svc *WAFService) AddCustomRuleSet(customRuleSet CustomRule, accountNumber 
 		return nil, fmt.Errorf("AddCustomRuleSet: %v", err)
 	}
 
-	parsedResponse := &AddCustomRuleResponse{}
+	parsedResponse := &AddCustomRuleSetResponse{}
 
 	_, err = svc.Client.SendRequest(request, &parsedResponse)
 
@@ -241,8 +252,24 @@ func (svc *WAFService) AddCustomRuleSet(customRuleSet CustomRule, accountNumber 
 	return parsedResponse, nil
 }
 
+// CustomRuleSetLight is a lightweight representation of a Custom Rule Set
+type CustomRuleSetLight struct {
+
+	// Indicates the system-defined ID for the custom rule set.
+	ID string `json:"id"`
+
+	// Indicates the date and time at which the custom rule was last modified.
+	//	Syntax:
+	// 		MM/DD/YYYYhh:mm:ss [AM|PM]
+	LastModifiedDate string `json:"last_modified_date"`
+
+	// Indicates the name of the custom rule set.
+	Name string `json:"name"`
+}
+
 // Retrieves a list of custom rule sets. A custom rule set allows you to define custom threat assessment criterion.
-func (svc *WAFService) GetAllCustomRuleSets(accountNumber string) ([]CustomRuleSet, error) {
+func (svc *WAFService) GetAllCustomRuleSets(accountNumber string) ([]CustomRuleSetLight, error) {
+
 	url := fmt.Sprintf("/v2/mcc/customers/%s/waf/v1.0/rules", accountNumber)
 
 	request, err := svc.Client.BuildRequest("GET", url, nil)
@@ -251,7 +278,7 @@ func (svc *WAFService) GetAllCustomRuleSets(accountNumber string) ([]CustomRuleS
 		return nil, fmt.Errorf("GetAllCustomRuleSets: %v", err)
 	}
 
-	var customRuleSets = &[]CustomRuleSet{}
+	var customRuleSets = &[]CustomRuleSetLight{}
 
 	_, err = svc.Client.SendRequest(request, &customRuleSets)
 
@@ -262,8 +289,18 @@ func (svc *WAFService) GetAllCustomRuleSets(accountNumber string) ([]CustomRuleS
 	return *customRuleSets, nil
 }
 
+// DeleteCustomRuleSetResponse represents the response from the WAF API when deleting a custom rule set
+type DeleteCustomRuleSetResponse struct {
+
+	// ID indicates the generated ID for the newly deleted Rule
+	ID string
+
+	WAFResponse
+}
+
 // Deletes a custom rule.
-func (svc *WAFService) DeleteCustomRuleSet(accountNumber string, customRuleID string) (*DeleteCustomRuleResponse, error) {
+func (svc *WAFService) DeleteCustomRuleSet(accountNumber string, customRuleID string) (*DeleteCustomRuleSetResponse, error) {
+
 	url := fmt.Sprintf("/v2/mcc/customers/%s/waf/v1.0/rules/%v", accountNumber, customRuleID)
 
 	request, err := svc.Client.BuildRequest("DELETE", url, nil)
@@ -272,12 +309,48 @@ func (svc *WAFService) DeleteCustomRuleSet(accountNumber string, customRuleID st
 		return nil, fmt.Errorf("DeleteCustomRuleSet: %v", err)
 	}
 
-	parsedResponse := &DeleteCustomRuleResponse{}
+	parsedResponse := &DeleteCustomRuleSetResponse{}
 
 	_, err = svc.Client.SendRequest(request, &parsedResponse)
 
 	if err != nil {
 		return nil, fmt.Errorf("DeleteCustomRuleSet: %v", err)
+	}
+
+	return parsedResponse, nil
+}
+
+// GetCustomRuleSetResponse represents the response from the WAF API when retrieving a custom rule set
+type GetCustomRuleSetResponse struct {
+
+	// ID indicates the generated ID for the newly deleted Rule
+	ID string
+
+	CustomRuleSetDetail
+
+	// Indicates the date and time at which the custom rule was last modified.
+	//	Syntax:
+	// 		MM/DD/YYYYhh:mm:ss [AM|PM]
+	LastModifiedDate string `json:"last_modified_date"`
+}
+
+// GetCustomRuleSet retrieves a custom rule.
+func (svc *WAFService) GetCustomRuleSet(accountNumber string, customRuleID string) (*GetCustomRuleSetResponse, error) {
+
+	url := fmt.Sprintf("/v2/mcc/customers/%s/waf/v1.0/rules/%v", accountNumber, customRuleID)
+
+	request, err := svc.Client.BuildRequest("GET", url, nil)
+
+	if err != nil {
+		return nil, fmt.Errorf("GetCustomRuleSet: %v", err)
+	}
+
+	parsedResponse := &GetCustomRuleSetResponse{}
+
+	_, err = svc.Client.SendRequest(request, &parsedResponse)
+
+	if err != nil {
+		return nil, fmt.Errorf("GetCustomRuleSet: %v", err)
 	}
 
 	return parsedResponse, nil
