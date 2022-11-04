78193a9bb88b732b9aff5e7c392c2cd0b16d3c02
wdawg33 <frank.contreras@verizondigitalmedia.com>
2022-11-04 15:52:05 -0700

    1) Addressed typos and code review feedback in Convert() 2) Refactored Convert() to improve unit testability 3) Added unit tests for Convert() 4) Added unit tests for Platform enums

--- edgecast/ecutils/convert.go
+++ edgecast/ecutils/convert.go
@@ -7,9 +7,14 @@ import (
 	"reflect"
 )
 
+var (
+	jsonMarshal   = json.Marshal
+	jsonUnmarshal = json.Unmarshal
+)
+
 // This function is used to convert a struct to the provided type.
-// If dest must be a pointer otherwise an error is returned.
-// Please refer to the json unmashall documentation for additional information
+// Dest must be a pointer otherwise an error is returned.
+// Please refer to the json unmarshall documentation for additional information
 // on expected behavior.
 //
 // Field names need to match between the source and destination structs. This
@@ -17,18 +22,18 @@ import (
 func Convert(src, dest any) error {
 	rv := reflect.ValueOf(dest)
 	if rv.Kind() != reflect.Pointer || rv.IsNil() {
-		return errors.New("dest must be a pointer")
+		return errors.New("dest must be a pointer and not nil")
 	}
 
 	// Marshal src to json bytes
-	srcBytes, err := json.Marshal(src)
+	srcBytes, err := jsonMarshal(src)
 	if err != nil {
 		return fmt.Errorf(
-			"unable to marshal get origin group response: %w", err)
+			"unable to marshal src: %s err: %w", src, err)
 	}
 
 	// Unmarshal into dest
-	err = json.Unmarshal(srcBytes, dest)
+	err = jsonUnmarshal(srcBytes, dest)
 	if err != nil {
 		return fmt.Errorf("failed to convert %T to %T: %w", src, dest, err)
 	}
